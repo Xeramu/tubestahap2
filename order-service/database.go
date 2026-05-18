c18d2de21c4711e3a7416141beaca55975477a51
Xeramu <umarexandromeda@upi.edu>
2026-05-18 10:23:38 +0700

    Update database.go

--- order-service/database.go
+++ order-service/database.go
@@ -1,55 +1,101 @@
+// package main
+
+// import (
+// 	"database/sql"
+// 	"fmt"
+
+// 	_ "github.com/go-sql-driver/mysql"
+// )
+
+// var DB *sql.DB
+
+// func ConnectDB() {
+
+// 	dsn := "root:root@tcp(mysql:3306)/tubesdb"
+
+// 	db, err := sql.Open("mysql", dsn)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	err = db.Ping()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	DB = db
+
+// 	createTable()
+
+// 	fmt.Println("ORDER DB CONNECTED")
+// }
+
+// func createTable() {
+
+// 	query := `
+// 	CREATE TABLE IF NOT EXISTS orders (
+// 		order_id INT AUTO_INCREMENT PRIMARY KEY,
+// 		user_id INT,
+// 		resi VARCHAR(100),
+// 		nama_barang VARCHAR(100),
+// 		berat INT,
+// 		dimensi VARCHAR(100),
+// 		jenis VARCHAR(100),
+// 		alamat_pengirim TEXT,
+// 		alamat_penerima TEXT,
+// 		status VARCHAR(50),
+// 		eta VARCHAR(100)
+// 	)
+// 	`
+
+// 	_, err := DB.Exec(query)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
 package main
 
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var DB *sql.DB
+func connectDB() (*sql.DB, error) {
 
-func ConnectDB() {
+	host := os.Getenv("DB_HOST")
 
-	dsn := "root:root@tcp(mysql:3306)/tubesdb"
+	// kalau DB_HOST kosong
+	if host == "" {
+		host = "localhost"
+	}
+
+	user := "root"
+	password := "password"
+	dbname := "orderdb"
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:3306)/%s",
+		user,
+		password,
+		host,
+		dbname,
+	)
 
 	db, err := sql.Open("mysql", dsn)
+
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	err = db.Ping()
+
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	DB = db
-
-	createTable()
-
-	fmt.Println("ORDER DB CONNECTED")
-}
-
-func createTable() {
-
-	query := `
-	CREATE TABLE IF NOT EXISTS orders (
-		order_id INT AUTO_INCREMENT PRIMARY KEY,
-		user_id INT,
-		resi VARCHAR(100),
-		nama_barang VARCHAR(100),
-		berat INT,
-		dimensi VARCHAR(100),
-		jenis VARCHAR(100),
-		alamat_pengirim TEXT,
-		alamat_penerima TEXT,
-		status VARCHAR(50),
-		eta VARCHAR(100)
-	)
-	`
-
-	_, err := DB.Exec(query)
-	if err != nil {
-		panic(err)
-	}
+	return db, nil
 }
