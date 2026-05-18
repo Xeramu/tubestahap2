5cdc801f9bcaa6e47cb49a78d5afd3856b4d4ac2
Xeramu <umarexandromeda@upi.edu>
2026-05-18 09:50:10 +0700

    Update service.go

--- order-service/service.go
+++ order-service/service.go
@@ -171,60 +171,57 @@
 
 
 
-package main
+// package main
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-	"time"
-)
+// import (
+// 	"errors"
+// 	"fmt"
+// 	"net/http"
+// 	"time"
+// )
 
-var orders []Order
-var nextID = 1
+// var orders []Order
+// var nextID = 1
 
-var UserServiceURL = "http://user-service:8081"
+// var UserServiceURL = "http://user-service:8081"
 
-type Validator interface {
-	CheckUser(userID int, token string) bool
-}
+// type Validator interface {
+// 	CheckUser(userID int, token string) bool
+// }
 
-type RealValidator struct{}
+// type RealValidator struct{}
 
-func (v RealValidator) CheckUser(userID int, token string) bool {
+// func (v RealValidator) CheckUser(userID int, token string) bool {
 
-	req, _ := http.NewRequest(
-		"GET",
-		fmt.Sprintf("%s/profile?id=%d", UserServiceURL, userID),
-		nil,
-	)
+// 	req, _ := http.NewRequest(
+// 		"GET",
+// 		fmt.Sprintf("%s/profile?id=%d", UserServiceURL, userID),
+// 		nil,
+// 	)
 
-	req.Header.Set("Authorization", "Bearer "+token)
+// 	req.Header.Set("Authorization", "Bearer "+token)
 
-	client := &http.Client{
-		Timeout: 3 * time.Second,
-	}
+// 	client := &http.Client{
+// 		Timeout: 3 * time.Second,
+// 	}
 
-	resp, err := client.Do(req)
+// 	resp, err := client.Do(req)
 
-	if err != nil {
-		return false
-	}
+// 	if err != nil {
+// 		return false
+// 	}
 
-	return resp.StatusCode == 200
-}
+// 	return resp.StatusCode == 200
+// }
 
-func GenerateResi() string {
-	return fmt.Sprintf("LNG-%d", time.Now().Unix())
-}
+// func GenerateResi() string {
+// 	return fmt.Sprintf("LNG-%d", time.Now().Unix())
+// }
 
-func CalculateETA() string {
-	return "2 days"
-}
+// func CalculateETA() string {
+// 	return "2 days"
+// }
 
-func CreateOrder(req Order, token string, v Validator) (Order, error) {
-	return User{}, nil
-}
 // func CreateOrder(req Order, token string, v Validator) (Order, error) {
 
 // 	if req.Berat <= 0 {
@@ -275,26 +272,92 @@ func CreateOrder(req Order, token string, v Validator) (Order, error) {
 // 	return req, nil
 // }
 
-func GetOrder(id int) *Order {
+// func GetOrder(id int) *Order {
 
-	for _, o := range orders {
-		if o.OrderID == id {
-			return &o
-		}
+// 	for _, o := range orders {
+// 		if o.OrderID == id {
+// 			return &o
+// 		}
+// 	}
+
+// 	return nil
+// }
+
+// func UpdateOrderStatus(id int, status string) bool {
+
+// 	for i := range orders {
+// 		if orders[i].OrderID == id {
+// 			orders[i].Status = status
+// 			return true
+// 		}
+// 	}
+
+// 	return false
+// }
+
+// func GetETA(id int) string {
+// 	return ""
+// }
+
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var orders []Order
+var nextID = 1
+
+var UserServiceURL = "http://user-service:8081"
+
+type Validator interface {
+	CheckUser(userID int, token string) bool
+}
+
+type RealValidator struct{}
+
+func (v RealValidator) CheckUser(userID int, token string) bool {
+	req, _ := http.NewRequest(
+		"GET",
+		fmt.Sprintf("%s/profile?id=%d", UserServiceURL, userID),
+		nil,
+	)
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{Timeout: 3 * time.Second}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("PROFILE ERROR:", err)
+		return false
 	}
 
+	fmt.Println("PROFILE STATUS:", resp.StatusCode)
+
+	return resp.StatusCode == 200
+}
+
+func GenerateResi() string {
+	return ""
+}
+
+func CalculateETA() string {
+	return ""
+}
+
+func CreateOrder(req Order, token string, v Validator) (Order, error) {
+	return Order{}, nil
+}
+
+func GetOrder(id int) *Order {
 	return nil
 }
 
 func UpdateOrderStatus(id int, status string) bool {
-
-	for i := range orders {
-		if orders[i].OrderID == id {
-			orders[i].Status = status
-			return true
-		}
-	}
-
 	return false
 }
 
