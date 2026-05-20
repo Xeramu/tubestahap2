6fac24145e20f000738e9023183ae2f928d09b4d
Xeramu <umarexandromeda@upi.edu>
2026-05-20 19:37:10 +0700

    Update database.go

--- user-service/database.go
+++ user-service/database.go
@@ -1,59 +1,10 @@
-// package main
-
-// import (
-// 	"database/sql"
-// 	"fmt"
-
-// 	_ "github.com/go-sql-driver/mysql"
-// )
-
-// var DB *sql.DB
-
-// func ConnectDB() {
-
-// 	dsn := "root:root@tcp(mysql:3306)/tubesdb"
-
-// 	db, err := sql.Open("mysql", dsn)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	err = db.Ping()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	DB = db
-
-// 	createTable()
-// 	fmt.Println("USER DB CONNECTED")
-// }
-
-// func createTable() {
-
-// 	query := `
-// 	CREATE TABLE IF NOT EXISTS users (
-// 		user_id INT AUTO_INCREMENT PRIMARY KEY,
-// 		name VARCHAR(100),
-// 		email VARCHAR(100),
-// 		password VARCHAR(255),
-// 		role VARCHAR(50),
-// 		alamat TEXT,
-// 		preferensi TEXT
-// 	)
-// 	`
-
-// 	_, err := DB.Exec(query)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
 package main
 
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -62,22 +13,45 @@ var DB *sql.DB
 
 func ConnectDB() {
 
-	dsn := "root:root@tcp(mysql:3306)/tubesdb"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
 
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		panic(err)
+	if host == "" {
+		host = "mysql"
 	}
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
+	if port == "" {
+		port = "3306"
 	}
 
-	DB = db
+	dsn := fmt.Sprintf(
+		"root:root@tcp(%s:%s)/tubesdb",
+		host,
+		port,
+	)
 
-	createTable()
-	fmt.Println("USER DB CONNECTED")
+	var err error
+
+	for i := 0; i < 10; i++ {
+
+		DB, err = sql.Open("mysql", dsn)
+
+		if err == nil {
+			err = DB.Ping()
+		}
+
+		if err == nil {
+			fmt.Println("USER DB CONNECTED")
+			createTable()
+			return
+		}
+
+		fmt.Println("Waiting MySQL...", err)
+
+		time.Sleep(5 * time.Second)
+	}
+
+	panic(err)
 }
 
 func createTable() {
